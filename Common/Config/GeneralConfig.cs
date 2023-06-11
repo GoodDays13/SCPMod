779b13bf86d8fee8a17a3a065b826e2a299428c0
Good_Days13 <42877449+GoodDays13@users.noreply.github.com>
2023-06-11 02:00:15 -0400

    Fixes some localization stuff

--- Common/Config/GeneralConfig.cs
+++ Common/Config/GeneralConfig.cs
@@ -8,19 +8,13 @@ namespace SCPMod.Common.Config
         public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [Header("SCP173")]
-        [Label("Invincible")]
-        [Tooltip("Should 173 be invincible?")]
         [DefaultValue(true)]
         public bool invincible173;
 
-        [Label("Damaging pickaxe power")]
-        [Tooltip("Pickaxe power needed to hurt 173")]
         [DefaultValue(110)]
         [Range(0, int.MaxValue)]
         public int pickToDamage173;
 
-        [Label("Moves in darkness")]
-        [Tooltip("Can 173 move in the pitch black?")]
         [DefaultValue(true)]
         public bool moveInDarkness173;
     }
